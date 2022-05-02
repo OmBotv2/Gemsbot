aa85f5df93e2d2e262a622dcd050335ec2f04aa5
OmBotv2 <103894626+OmBotv2@users.noreply.github.com>
2022-05-02 08:18:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['919971107409', 'Valor_Bot', true]
+  ['6282239202895', 'Alwi_ofc', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
-global.mods = ['919971107409'] // Want some help?
-global.prems = ['919971107409'] // Premium user has unlimited limit
+global.mods = ['6282239202895'] // Want some help?
+global.prems = ['6282239202895'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -28,8 +28,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'Valor Bot'
-global.author = 'Dinesh_Valor🇮🇳'
+global.packname = 'Hikari-md'
+global.author = '©by Alwi ofc'
 
 global.multiplier = 69 // The higher, The harder levelup
 
