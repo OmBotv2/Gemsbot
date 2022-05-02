88bd1b515e634d8cf2f6302106bba2652d424397
OmBotv2 <103894626+OmBotv2@users.noreply.github.com>
2022-05-02 15:40:25 +0800

    Update info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -73,7 +73,7 @@ let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
 ⮕ ${groupsIn.length - groupsIn.length} - Groups Left
 ⮕ ${chats.length - groupsIn.length} - Personal Chats
 ⮕ ${chats.length} - Total Chats
-`.trim(), './media/botinfo.jpg', 'wa.me/6282239202895',Owner', null, null, [
+`.trim(), './media/botinfo.jpg', 'wa.me/6282239202895','Owner', null, null, [
 [`ᴏᴡɴᴇʀ`, `${usedPrefix}owner`],
 [`ᴅᴏɴᴀᴛᴇ`, `${usedPrefix}donate`]
 ], m, {asLocation: true})
